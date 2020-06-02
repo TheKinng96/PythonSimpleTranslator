c0bac659102e453e7cdf122b723f45c73fbd0204
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-06-02 10:37:17 +0900

    finished with an output function of the translation

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from translate import Translator
 
+translator = Translator(to_lang="ja")
+
 try:
     with open('./try.txt', mode='r') as my_file:
-        print(my_file.read())
+        text = my_file.read()
+        translation = translator.translate(text)
+        with open('./test-ja.txt', mode='w') as my_file2:
+            my_file2.write(translation)
 except FileNotFoundError as e:
     print('Please check your file path.')
